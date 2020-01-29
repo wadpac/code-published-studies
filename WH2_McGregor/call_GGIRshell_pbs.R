75c33751906c99af7641040649476c065983b2d1
Vincent van Hees <vincentvanhees@gmail.com>
2020-01-29 10:58:24 -0500

    first commit

---
+++ WH2_McGregor/call_GGIRshell_pbs.R
@@ -0,0 +1,177 @@
+#!/usr/bin/Rscript
+# R script to run analyses for the Whitehall study II, by Vincent van Hees
+#==================================================================
+# INPUT NEEDED:
+
+.libPaths('/gpfs/ts0/home/vv233/R/x86_64-pc-linux-gnu-library/3.5') # location of R packages
+
+
+# list.of.packages <- c("data.table", "Rcpp","zoo", "bitops", "matlab", "signal", "tuneR", "mmap", "GENEAread")
+# new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+# if(length(new.packages)) install.packages(new.packages)
+
+
+location_configfile = "/gpfs/mrc0/projects/Research_Project-MRC158833/vv233/whitehall_pbs/config.txt" # location of config file
+
+#=====================================================================================
+options(echo=TRUE)
+args = commandArgs(TRUE)
+cat(args)
+if(length(args) > 0) {
+  for (i in 1:length(args)) {
+    eval(parse(text = args[[i]]))
+  }
+}
+
+# specify file number to start and end with, fill in c() if unknown
+# What part of the analysis needs to be done (options: 1,2,3,4 and 5)
+if (mode == 0) {
+  do.report = c(2,4,5)
+  mode = c()
+} else {
+  do.report = c()
+}
+if (overwrite == 0) {
+  overwrite = FALSE
+} else {
+  overwrite = TRUE
+}
+# specifiy the location of the config file:
+config = read.csv(location_configfile,row.names = 1,stringsAsFactors = FALSE,sep = ",",strip.white = TRUE)
+datadir = config$datadir
+outputdir = config$outputdir
+studyname = config$studyname
+dirR = config$dirR
+loglocation = config$loglocation
+
+
+# load functions directly from local clone of the R package repository
+ffnames = dir(dirR) # creating list of filenames of scriptfiles to load
+cat("\n")
+cat(dirR)
+cat("\n")
+cat(ffnames)
+for (i in 1:length(ffnames)) {
+  source(paste(dirR,"/",ffnames[i],sep="")) #loading scripts for reading geneactiv data
+}
+
+library("mmap")
+library("GENEAread")
+library("bitops")
+library("matlab")
+library("signal")
+library("tuneR")
+library("zoo")
+library("data.table")
+
+g.shell.GGIR(#=======================================
+             # INPUT NEEDED:
+             #-------------------------------
+             # General parameters
+             #-------------------------------
+             mode=mode, #specify above
+             datadir=datadir, #specify above
+             outputdir=outputdir, #specify above
+             studyname=studyname, #specify above
+             f0=f0, #specify above
+             f1=f1, #specify above
+             overwrite = FALSE, #overwrite previous milestone data?
+             do.imp=TRUE, # Do imputation? (recommended)
+             idloc=2, #id location (1 = file header, 2 = filename)
+             print.filename=TRUE,
+             storefolderstructure = TRUE,
+             #-------------------------------
+             # Part 1 parameters:
+             #-------------------------------
+             # Key functions: reading file, auto-calibration, and extracting features
+             windowsizes = c(5,900,3600), #Epoch length, non-wear detection resolution, non-wear detection evaluation window
+             do.cal= TRUE, # Apply autocalibration? (recommended)
+             do.enmo = TRUE, #Needed for physical activity analysis
+             do.anglez=TRUE, #Needed for sleep detection
+             #do.angley=TRUE,
+             #do.anglex=TRUE,
+             #do.roll_med_acc_x=TRUE,
+             #do.roll_med_acc_y=TRUE,
+             #do.roll_med_acc_z=TRUE,
+             #do.dev_roll_med_acc_x=TRUE,
+             #do.dev_roll_med_acc_y=TRUE,
+             #do.dev_roll_med_acc_z=TRUE,
+             
+             chunksize=1, #size of data chunks to be read (value = 1 is maximum)
+             printsummary=TRUE,
+             #-------------------------------
+             # Part 2 parameters:
+             #-------------------------------
+             # Key functions: Non-wear detection, imputation, and basic descriptives
+             strategy = 2, #Strategy (see tutorial for explanation)
+             #ndayswindow=7, #only relevant when strategy = 3
+             hrs.del.start = 0, # Only relevant when strategy = 2. How many HOURS need to be ignored at the START of the measurement?
+             hrs.del.end = 0, # Only relevant when strategy = 2. How many HOURS need to be ignored at the END of the measurement?
+             maxdur = 9, # How many DAYS of measurement do you maximumally expect?
+             includedaycrit = 16, # number of minimum valid hours in a day to attempt physical activity analysis
+             M5L5res = 10, #resolution in minutes of M5 and L5 calculation
+             winhr = c(5,10), # size of M5 and L5 (5 hours by default)
+             
+             qlevels = c(c(1380/1440),c(1410/1440)), #quantiles to calculate, set value at c() if you do not want quantiles
+             qwindow=c(0,24), #window over which to calculate quantiles
+             ilevels = c(seq(0,400,by=50),8000), #acceleration values (metric ENMO) from which a frequency distribution needs to be derived, set value at c() if you do not want quantiles
+             iglevels = TRUE, # intensitygradient levels
+             mvpathreshold =c(100,120), #MVPA (moderate and vigorous physical activity threshold
+             bout.metric = 4,
+             closedbout=FALSE,
+             IVIS_windowsize_minutes = 60,
+             IVIS_epochsize_seconds = 3600,
+             #-------------------------------
+             # Part 3 parameters:
+             #-------------------------------
+             # Key functions: Sleep detection
+             timethreshold= c(5), #10
+             anglethreshold=5,
+             ignorenonwear = TRUE, # if TRUE non-wear is not detected as sleep (if FALSE then it will work with imputed data)
+             constrain2range = TRUE,
+             do.part3.pdf = FALSE,
+             #-------------------------------
+             # Part 4 parameters:
+             #-------------------------------
+             # Key functions: Integrating sleep log (if available) with sleep detection, storing day and person specific summaries of sleep
+             excludefirstlast = TRUE, # Exclude first and last night for sleep analysis?
+             includenightcrit = 16, # number of minimum valid hours in a day to attempt sleep analysis
+             def.noc.sleep = c(),
+             # If sleep log is available:
+             loglocation= loglocation, # full directory and name of the log (if available, otherwise leave value as c() )
+             outliers.only = TRUE,
+             criterror = 4,
+             relyonsleeplog = FALSE,
+             sleeplogidnum = TRUE, # Is the participant in the sleep log stored as a number (TRUE) or as a character (FALSE)
+             colid=1, #colomn in which the participant id or filename is stored
+             coln1=2, #column number for first day
+             do.visual = TRUE,
+             nnights = 9, #number of nights in the sleep log
+             #-------------------------------
+             # Part 5 parameters:
+             #-------------------------------
+             # Key functions: Merging physical activity with sleep analyses
+             threshold.lig = c(30,40), #threshold(s) for inactivity (can be more than one number)
+             threshold.mod = c(100,120), #threshold(s) for moderate activity (can be more than one number)
+             threshold.vig = c(400), #threshold(s) for vigorous activity (can be more than one number)
+             boutcriter = 0.8,
+             boutcriter.in = 0.8, #fraction of an inactivity bout that needs to be below the threshold (needs to be 1 number)
+             boutcriter.lig = 0.8, #fraction of an light activity bout that needs to be between the thresholds (needs to be 1 number)
+             boutcriter.mvpa = 0.8, #fraction of an light activity bout that needs to be above the threshold (needs to be 1 number)
+             boutdur.in = c(1,10,30,60), # duration of bouts to be calculated
+             boutdur.lig = c(1,10), # duration of bouts to be calculated
+             boutdur.mvpa = c(1,10), # duration of bouts to be calculated
+             timewindow = c("WW","MM"), #,
+#             do.LIDS = TRUE,
+#             LIDS2csv = TRUE,
+ #            LIDS_cosfit_periods = seq(30,180, by =5),
+#             nonstationary = FALSE,
+#             WakeBouteMin = 30, SleepBoutMin = 180,
+             #-----------------------------------
+             # Report generation
+             #-------------------------------
+             # Key functions: Generating reports based on meta-data
+             do.report=do.report, #for what parts does and report need to be generated? (option: 2, 4 and 5)
+             visualreport=FALSE,
+             dofirstpage = TRUE, #first page of pdf-report with simple summary histograms
+             viewingwindow=1) #viewingwindow of visual report: 1 centres at day and 2 centers at night
